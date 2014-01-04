a5ee9fccef9dcaef1488bd9ea3b8a3e66d8afc0b
Alexander Popolitov <popolit@gmail.com>
2014-01-04 16:46:38 +0000

    First working crappy version of splicing macro

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ This package adds a possibility for macros to inject several forms
 CL-SPLICING-MACRO> (defmacro bar (&rest things)
                      "Just a toy macros, that expands into a PLUS"
                      `(+ ,@things))
-CL-SPLICING-MACRO> (define-maybe-splicing-macro foo (a b)
+CL-SPLICING-MACRO> (define-maybe-splicing-macro foo (a b &sample (1 2))
                      "Just a toy macro that will splice in the body of surrounding macro."
                      `(sprogn ,a ,b))
 CL-SPLICING-MACRO> (macroexpand-1 '(bar 1 2 (foo 3 4) 5 6))
--- cl-splicing-macro.lisp
+++ cl-splicing-macro.lisp
@@ -6,13 +6,68 @@
   (declare (ignore forms))
   (error "This macro is not meant to be expanded directly"))
 
-(defvar *splicing-macro* (make-hash-table :test #'eq))
+(defvar *splicing-macro* (make-hash-table))
+(defvar *sample-expansion-data* (make-hash-table))
 
 (defun mk-splicing-macroexpand-hook (old-macroexpand-hook)
   (lambda (fun form lexenv)
-    (format t "I'm a loud macroexpand hook!~%")
-    (funcall old-macroexpand-hook fun form lexenv)))
+    ;; We need to treat compiler macro separately, cause they seem
+    ;; to cause infinite macroexpansion loops otherwise
+    (if (eq (compiler-macro-function (car form) lexenv)
+	    fun)
+	(funcall old-macroexpand-hook fun form lexenv)
+	(funcall old-macroexpand-hook fun
+		 (let (res)
+		   (dolist (subform (cdr form) (cons (car form)
+						     (reverse res)))
+		     (if (and (listp subform)
+			      (gethash (car subform) *splicing-macro*))
+			 (dolist (elt (cdr (macroexpand-1 subform lexenv)))
+			   (push elt res))
+			 (push subform res))))
+		 lexenv))))
 
+(defun ampersand-symbol-p (sym)
+  (and (symbolp sym)
+       (not (keywordp sym))
+       (char= #\& (char (string sym) 0))))
+
+(defun parse-sample-from-lambda-list (lst)
+  (let (res got-sample sample)
+    (do ((cur lst (cdr cur)))
+	((endp cur))
+      (if (and (not got-sample)
+	       (symbolp (car cur))
+	       (string= "&SAMPLE" (string (car cur))))
+	  (progn (setf cur (cdr cur))
+		 (if (or (not cur) (ampersand-symbol-p (car cur)))
+		     (error "Sample specification is missing after &SAMPLE magic word.")
+		     (setf sample (car cur)
+			   got-sample t))
+		 (assert (listp sample)))
+	  (push (car cur) res)))
+    (values (nreverse res) sample got-sample)))
+
+(defmacro define-maybe-sampling-macro (name lambda-list &rest body)
+  (multiple-value-bind (residual-list sample got) (parse-sample-from-lambda-list lambda-list)
+    `(progn (defmacro ,name ,residual-list ,@body)
+	    ,(if got
+		 `(setf (gethash ',name *sample-expansion-data*) 
+			',sample)))))
+		
+(defmacro testing-expansion (name &environment env)
+  (multiple-value-bind (sample-expansion-data got) (gethash name *sample-expansion-data*)
+     (if (not got)
+	 `(values nil nil)
+	 `(values ',(macroexpand-1 `(,name ,@sample-expansion-data) env)
+		  t))))
+
+(defmacro define-maybe-splicing-macro (name lambda-list &rest body)
+  `(progn (define-maybe-sampling-macro ,name ,lambda-list ,@body)
+	  (multiple-value-bind (expansion has-sample-expansion) (testing-expansion ,name)
+	    (if (and has-sample-expansion
+		     (eq (car expansion) 'sprogn))
+		(setf (gethash ',name *splicing-macro*) t)))))
 
 (let (installed)
   (defun enable-splicing-macro ()
@@ -28,3 +83,7 @@
 	       t)
 	(format t "Splicing macro already disabled, doing nothing."))))
 
+;; OK, now that I have a basic skeleton of my project, time to
+;; write something nontrivial!)
+
+;; OK, I need to implement sample macroexpansion for MAYBE-SPLICING to work
